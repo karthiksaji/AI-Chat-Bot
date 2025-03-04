ff0a1497de4d724fc6ac32c27f9f9e0ad0f66c85
sajeevkarthik87@gmail.com <kl05au@8938>
2025-03-04 15:40:57 +0530

    start

---
+++ src/components/ChatBotStart.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ChatBotStart.css";
+
+const ChatBotStart = ({ onStartChat }) => {
+  return (
+    <div classname="start-page">
+      <button className="start-page-button" onClick={onStartChat}>
+        Chat AI
+      </button>
+      ;
+    </div>
+  );
+};
+
+export default ChatBotStart;
