ee327c85f276a0f6e1df892a5836dd3530fbde7f
sajeevkarthik87@gmail.com <kl05au@8938>
2025-03-04 13:10:14 +0530

    update chatbot start name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import ChatBotStart from "./Components/chatbotstart";
+import ChatBotStart from "./Components/ChatBotStart";
 import ChatBotApp from "./Components/ChatBotApp";
 const App = () => {
   const [isChatting, setIsChatting] = useState(false);
